ccc3fa9c978fb530488f6fecb1c348ac1dbcf0f8
zohaibhassan9 <zabaidafashion@gmail.com>
2025-02-20 16:12:33 +0500

    Code working smoothly

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const choices = ["rock", "paper", "scissors"];
+const result = document.getElementById("result");
+const playerChoice = document.getElementById("playerChoice");
+const computerChoice  = document.getElementById("computerChoice");
+
+function playGame(playerSelection){
+let randomIndex = Math.floor(Math.random() * 3);
+let computerSelection = choices[randomIndex];
+
+playerChoice.textContent = playerSelection;
+computerChoice.textContent = computerSelection;
+
+let resultWinner = getWinner(playerSelection, computerSelection)
+result.textContent = resultWinner;
+
+}
+
+
 function getWinner(player, computer){
 if(player === computer){
 return "It's a tie 🤝"
